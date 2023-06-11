388c16bf0173d4e841b79123d306aa67e9b1c87d
Mohamed Ali <mali@youxel.com>
2023-06-11 06:48:46 +0300

    update

--- src/convert-to-short.ts
+++ src/convert-to-short.ts
@@ -1,14 +1,28 @@
 import ffmpeg from "fluent-ffmpeg";
+import ffmpegStatic from "ffmpeg-static";
+
 import ffmpegPath from "@ffmpeg-installer/ffmpeg";
 
+
 // Set the path for ffmpeg
-ffmpeg.setFfmpegPath(ffmpegPath.path);
 
 export let convertToYoutubeShorts = async (
-  input: string,
+  input: string, 
   output: string
 ): Promise<void> => {
-    
+  return new Promise((resolve, reject) => {
+    let process = ffmpeg(input)
+      .setDuration(60)       // Max duration is 60s
+      .aspect('9:16')
+      .format('mp4')       
+      .outputOptions([
+        '-movflags +faststart' // Put moov atom at the begining of the file
+      ])
+      .output(output)      
+      .on('end', resolve)
+      .on('error', reject)
+      .run();
+  });
 };
 
 (async () => {
